166005b0942f06f33394f1775278e0e7b76c92e1
915-Nistor-David <david.cristian2003@gmail.com>
2025-01-07 18:13:49 +0200

    StartTCP

--- p2p.c
+++ p2p.c
@@ -12,11 +12,11 @@ typedef struct {
     uint16_t port; // Portul peer-ului
 } PeerInfo;
 
-// Lista globală de peers pentru server
+// Lista globala de peers pentru server
 PeerInfo peers[MAX_PEERS];
 int peer_count = 0;
 
-// Adaugă un peer în lista globală (doar pentru testare)
+// Adauga un peer in lista globala (doar pentru testare)
 void add_peer(const char *ip, uint16_t port) {
     if (peer_count >= MAX_PEERS) {
         fprintf(stderr, "Lista de peers este plină!\n");
@@ -30,7 +30,7 @@ void add_peer(const char *ip, uint16_t port) {
 }
 
 
-// Funcția care gestionează cererile `PEER_DISCOVERY`
+// Functia care gestionează cererile `PEER_DISCOVERY`
 void handle_peer_discovery(int client_socket) {
     char buffer[BUFFER_SIZE];
     recv(client_socket, buffer, sizeof(buffer), 0);
@@ -47,4 +47,54 @@ void handle_peer_discovery(int client_socket) {
     }
 
     close(client_socket);
+}
+
+
+
+// Functia care pornește serverul TCP
+void start_tcp_server(int port) {
+    int server_socket, client_socket;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t addr_len = sizeof(client_addr);
+
+    // Creare socket
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        perror("Eroare la crearea socket-ului");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configurare adresa server
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+
+    // Legare socket de port
+    if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Eroare la bind");
+        close(server_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    // Asteptare conexiuni
+    if (listen(server_socket, 5) == -1) {
+        perror("Eroare la listen");
+        close(server_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("TCP Server pornit pe portul %d...\n", port);
+
+    // Acceptare conexiuni
+    while (1) {
+        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &addr_len);
+        if (client_socket == -1) {
+            perror("Eroare la accept");
+            continue;
+        }
+
+        handle_peer_discovery(client_socket);
+    }
+
+    close(server_socket);
 }
