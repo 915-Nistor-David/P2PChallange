c77af7e483177567571ae43b8334aea80c53adc8
915-Nistor-David <david.cristian2003@gmail.com>
2025-01-07 18:03:05 +0200

    GestionarePeer

--- p2p.c
+++ p2p.c
@@ -28,3 +28,23 @@ void add_peer(const char *ip, uint16_t port) {
     peer.port = port;
     peers[peer_count++] = peer;
 }
+
+
+// Funcția care gestionează cererile `PEER_DISCOVERY`
+void handle_peer_discovery(int client_socket) {
+    char buffer[BUFFER_SIZE];
+    recv(client_socket, buffer, sizeof(buffer), 0);
+
+    if (strncmp(buffer, "PEER_DISCOVERY", 14) == 0) {
+        // Trimite lista de peers
+        uint64_t vector_size = peer_count;
+        send(client_socket, "PEER_LIST", 9, 0);
+        send(client_socket, &vector_size, sizeof(vector_size), 0);
+
+        for (int i = 0; i < peer_count; i++) {
+            send(client_socket, &peers[i], sizeof(PeerInfo), 0);
+        }
+    }
+
+    close(client_socket);
+}
